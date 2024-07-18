7940d43f34b5924c2a618912f24723fdb9607f32
Daffadon <daffaputranarendra9@gmail.com>
2024-07-18 09:28:18 +0700

    feat: add db initialization

---
+++ db/setup.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var db *gorm.DB
+
+func Init() {
+	if err := godotenv.Load(); err != nil {
+		panic("Failed to load .env file!")
+	}
+
+	user := os.Getenv("DATABASE_USERNAME")
+	password := os.Getenv("DATABASE_PASSWORD")
+	host := os.Getenv("DATABASE_URL")
+	port := os.Getenv("DATABASE_PORT")
+	dbname := os.Getenv("DATABASE_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbname)
+
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+	db = database
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
