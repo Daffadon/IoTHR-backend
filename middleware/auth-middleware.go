84bedb6b0f28902c7c425625f0f8f7f38b219b09
Daffadon <daffaputranarendra9@gmail.com>
2024-07-18 09:28:35 +0700

    feat: add auth and cors middleware

---
+++ middleware/auth-middleware.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"IoTHR-backend/models"
+	"IoTHR-backend/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+	
+func AuthMiddleware(ctx *gin.Context) {
+	token := ctx.GetHeader("Authorization")
+	if token != "" {
+		token = token[7:]
+		claims, err := utils.ValidateJWT(token)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized!"})
+			ctx.Abort()
+			return
+		}
+		loggedOut := models.IsLoggedOut(token)
+		if !loggedOut {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized!"})
+			ctx.Abort()
+			return
+		}
+		ctx.Set("user_id", claims.UserId)
+		ctx.Set("role", claims.Role)
+		ctx.Next()
+		return
+	}
+	ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized!"})
+	ctx.Abort()
+}
