eedcfb985a40e15e60bf63839e92b7c2ec9e1197
Daffadon <daffaputranarendra9@gmail.com>
2024-07-20 20:29:04 +0700

    feat:add function related to user and change user model struct

--- models/user-model.go
+++ models/user-model.go
@@ -2,68 +2,153 @@ package models
 
 import (
 	"IoTHR-backend/db"
-	"IoTHR-backend/utils"
 	"IoTHR-backend/validations"
+	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type User struct {
-	ID       uint   `json:"id" gorm:"primary_key"`
-	Email    string `json:"email" gorm:"unique;not null" `
-	Fullname string `json:"fullname" gorm:"not null"`
-	Password string `json:"password" gorm:"not null"`
-	Role     string `json:"role" gorm:"default:'user';not null"`
-	Token    string `json:"token" gorm:"default:null"`
+	ID       primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
+	Email    string               `json:"email" bson:"email"`
+	Fullname string               `json:"fullname" bson:"fullname"`
+	Password string               `json:"password" bson:"password"`
+	Role     string               `json:"role" bson:"role"`
+	Token    string               `json:"token,omitempty" bson:"token,omitempty"`
+	TopicID  []primitive.ObjectID `json:"topicId,omitempty" bson:"topicId,omitempty"`
+}
+
+func (u User) CreateUser(input *validations.CreateUserInput) (*User, error) {
+	userCollection := db.GetUserCollection()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	user := User{Fullname: input.Fullname, Email: input.Email, Password: input.Password, Role: "user"}
+
+	filter := bson.M{"email": input.Email}
+	var existingUser User
+	err := userCollection.FindOne(ctx, filter).Decode(&existingUser)
+	if err == nil {
+		return nil, fmt.Errorf("email already exists")
+	} else if err != mongo.ErrNoDocuments {
+		return nil, err
+	}
+
+	result, err := userCollection.InsertOne(ctx, user)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert user: %v", err)
+	}
+	insertedID, ok := result.InsertedID.(primitive.ObjectID)
+
+	if !ok {
+		return nil, fmt.Errorf("failed to get inserted ID")
+	}
+
+	user.ID = insertedID
+	return &user, nil
 }
 
 func (u User) GetUser(input *validations.LoginInput) (*User, error) {
-	db := db.GetDB()
+	userCollection := db.GetUserCollection()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	var user User
-	if err := db.Where("email = ?", input.Email).First(&user).Error; err != nil {
+	filter := bson.M{"email": input.Email}
+	err := userCollection.FindOne(ctx, filter).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, fmt.Errorf("user not found")
+		}
 		return nil, err
 	}
 	return &user, nil
 }
 
-func (u User) GetUserByID(id uint) (*User, error) {
-	db := db.GetDB()
+func (u User) GetUserByID(id primitive.ObjectID) (*User, error) {
+	userCollection := db.GetUserCollection()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	var user User
-	if err := db.Where("id = ?", id).First(&user).Error; err != nil {
+	filter := bson.M{"_id": id}
+	err := userCollection.FindOne(ctx, filter).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, fmt.Errorf("user not found")
+		}
 		return nil, err
 	}
 	return &user, nil
 }
 
-func (u User) UpdateTokenToUser(id uint, token string) error {
-	db := db.GetDB()
-	if err := db.Model(&User{}).Where("id = ?", id).Update("token", token); err != nil {
-		return err.Error
+func (u User) UpdateTokenToUser(userid primitive.ObjectID, token string) error {
+	userCollection := db.GetUserCollection()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	filter := bson.M{"_id": userid}
+	update := bson.M{"$set": bson.M{"token": token}}
+
+	_, err := userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return fmt.Errorf("error updating user token: %v", err)
 	}
 	return nil
 }
 
-func (u User) CreateUser(input *validations.CreateUserInput) (*User, error) {
-	db := db.GetDB()
-	user := User{Fullname: input.Fullname, Email: input.Email, Password: input.Password}
-	if err := db.Create(&user).Error; err != nil {
-		fmt.Println(err.Error())
-		if utils.IsUniqueConstraintError(err) {
-			return nil, fmt.Errorf("email already exists")
-		}
-	}
-	return &user, nil
-}
 func (u User) RemoveToken(input *validations.LogoutInput) error {
-	db := db.GetDB()
-	if err := db.Model(&User{}).Where("id = ?", input.Userid).Update("token", nil); err != nil {
-		return err.Error
+	userCollection := db.GetUserCollection()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	filter := bson.M{"_id": input.Userid}
+	update := bson.M{"$unset": bson.M{"token": ""}}
+
+	result, err := userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return fmt.Errorf("error updating user token: %v", err)
+	}
+
+	if result.ModifiedCount == 0 {
+		return fmt.Errorf("user not found or token already removed")
 	}
 	return nil
 }
 
 func IsLoggedOut(token string) bool {
-	var count int64
-	db := db.GetDB()
-	db.Model(&User{}).Where("token = ?", token).Count(&count)
+	collection := db.GetUserCollection()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	filter := bson.M{"token": token}
+
+	count, err := collection.CountDocuments(ctx, filter)
+	if err != nil {
+		log.Printf("Error counting documents: %v\n", err)
+		return false
+	}
 	return count > 0
 }
+
+func UpdateTopicID(userid primitive.ObjectID, topicid primitive.ObjectID) error {
+	userCollection := db.GetUserCollection()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	filter := bson.M{"_id": userid}
+	update := bson.M{"$push": bson.M{"topicId": topicid}}
+
+	_, err := userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return fmt.Errorf("error updating user topicId: %v", err)
+	}
+	return nil
+}
+
+
