230ecd5387720975514c9ca125ddff38e92139ef
Daffadon <daffaputranarendra9@gmail.com>
2024-07-20 20:27:38 +0700

    refactor: change database setup to NoSQL

--- db/setup.go
+++ db/setup.go
@@ -1,40 +1,44 @@
 package db
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var db *gorm.DB
+var db *mongo.Client
 
 func Init() {
 	if err := godotenv.Load(); err != nil {
 		panic("Failed to load .env file!")
 	}
 
-	user := os.Getenv("DATABASE_USERNAME")
-	password := os.Getenv("DATABASE_PASSWORD")
-	host := os.Getenv("DATABASE_URL")
-	port := os.Getenv("DATABASE_PORT")
-	dbname := os.Getenv("DATABASE_NAME")
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbname)
-
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
-
+	MONGO_URI := os.Getenv("MONGO_URI")
+	clientOptions := options.Client().ApplyURI(MONGO_URI)
+	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
-	db = database
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		panic("Failed to connect to database!")
+	} else {
+		fmt.Println("Connected to mongoDB!!!")
+	}
+	db = client
 }
 
-func GetDB() *gorm.DB {
+func GetUserCollection() *mongo.Collection {
+	return db.Database("heartrate").Collection("user")
+}
+func GetTopicCollection() *mongo.Collection {
+	return db.Database("heartrate").Collection("topic")
+}
+
+func GetDB() *mongo.Client {
 	return db
 }
