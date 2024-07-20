73ec64b56dfce2c5ded958105d094b2723714d07
Daffadon <daffaputranarendra9@gmail.com>
2024-07-20 20:30:42 +0700

    feat: add validation for history service

--- validations/profile-validation.go
+++ validations/profile-validation.go
@@ -1,6 +1,16 @@
 package validations
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Profile struct {
 	Email    string `json:"email" `
 	Fullname string `json:"fullname"`
+}
+type GetHistoryInput struct {
+	TopicList []primitive.ObjectID `json:"topicList"`
+}
+
+type HistoryReturn struct {
+	TopicId   primitive.ObjectID `json:"topicId"`
+	TopicName string             `json:"topicName"`
 }
