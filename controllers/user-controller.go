5d903383bbacfca8416ea1d3b9c59179ec076253
Daffadon <daffaputranarendra9@gmail.com>
2024-07-20 20:27:07 +0700

    feat: add get history to usercontroller

--- controllers/user-controller.go
+++ controllers/user-controller.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type UserController struct{}
@@ -12,11 +13,30 @@ type UserController struct{}
 func (u UserController) GetProfile(ctx *gin.Context) {
 	userID, ok := ctx.Get("user_id")
 	if ok {
-		if user, err := UserModel.GetUserByID(userID.(uint)); err == nil {
+		if user, err := UserModel.GetUserByID(userID.(primitive.ObjectID)); err == nil {
 			userData := &validations.Profile{
 				Email: user.Email, Fullname: user.Fullname,
 			}
 			ctx.JSON(http.StatusOK, gin.H{"data": userData})
+			ctx.Abort()
+			return
+		}
+	}
+	ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid user"})
+}
+
+func (u UserController) GetHistory(ctx *gin.Context) {
+	userID, ok := ctx.Get("user_id")
+	if ok {
+		if user, err := UserModel.GetUserByID(userID.(primitive.ObjectID)); err == nil {
+
+			topicNames, err := TopicModel.GetTopicList(&validations.GetHistoryInput{TopicList: user.TopicID})
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				ctx.Abort()
+				return
+			}
+			ctx.JSON(http.StatusOK, gin.H{"data": topicNames})
 			ctx.Abort()
 			return
 		}
