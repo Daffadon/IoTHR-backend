4365ab415a3ac8aa150bb37e5abec5a146ca7eb8
Daffadon <daffaputranarendra9@gmail.com>
2024-07-24 12:01:55 +0700

    feat: add user needed by app

--- db/setup.go
+++ db/setup.go
@@ -3,9 +3,12 @@ package db
 import (
 	"context"
 	"fmt"
+		"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -17,19 +20,48 @@ func Init() {
 		panic("Failed to load .env file!")
 	}
 
-	MONGO_URI := os.Getenv("MONGO_URI")
-	clientOptions := options.Client().ApplyURI(MONGO_URI)
+	MONGO_ADMIN_URI := os.Getenv("MONGO_ADMIN_URI")
+	clientOptions := options.Client().ApplyURI(MONGO_ADMIN_URI)
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
-	err = client.Ping(context.Background(), nil)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	adminDB := client.Database("heartrate")
+	newUser := os.Getenv("APP_DB_USERNAME")
+	password := os.Getenv("APP_DB_PASSWORD")
+
+	result := adminDB.RunCommand(context.TODO(), bson.D{
+		{Key: "createUser", Value: newUser},
+		{Key: "pwd", Value: password},
+		{Key: "roles", Value: bson.A{
+			bson.D{{Key: "role", Value: "readWrite"}, {Key: "db", Value: os.Getenv("DATABASE")}},
+			bson.D{{Key: "role", Value: "dbAdmin"}, {Key: "db", Value: os.Getenv("DATABASE")}},
+		}},
+	})
+	if err = result.Err(); err != nil {
+		log.Fatalf("Failed to create user: %v", err)
+	}
+	fmt.Println("User created successfully!")
+
+	client.Disconnect(context.Background())
+
+	MONGO_URI := os.Getenv("MONGO_URI")
+	hrClientOptions := options.Client().ApplyURI(MONGO_URI)
+	hrClient, err := mongo.Connect(context.Background(), hrClientOptions)
 	if err != nil {
 		panic("Failed to connect to database!")
-	} else {
-		fmt.Println("Connected to mongoDB!!!")
 	}
-	db = client
+
+	db = hrClient
 }
 
 func GetUserCollection() *mongo.Collection {
