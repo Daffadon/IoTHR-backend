3a9968b6a12ddcec47020ad569f0b3fcaf008bee
Daffadon <daffaputranarendra9@gmail.com>
2024-07-30 15:49:02 +0700

    refactor: change struct, prediction method and add update record time

--- models/topic-model.go
+++ models/topic-model.go
@@ -24,29 +24,8 @@ type Topic struct {
 	UserID     primitive.ObjectID `json:"userId,omitempty" bson:"userId,omitempty"`
 	Name       string             `bson:"name" json:"name"`
 	Date       time.Time          `bson:"date" json:"date"`
-	Prediction struct {
-		N float64 `bson:"N" json:"N"`
-		S float64 `bson:"S" json:"S"`
-		V float64 `bson:"V" json:"V"`
-		F float64 `bson:"F" json:"F"`
-		Q float64 `bson:"Q" json:"Q"`
-	} `bson:"prediction,omitempty" json:"prediction,omitempty"`
-	Feature    string    `bson:"feature,omitempty" json:"feature,omitempty"`
-	EcgPlot    []float64 `bson:"ecgplot,omitempty" json:"ecgplot,omitempty"`
-	Annotation struct {
-		N [][]float64 `bson:"N" json:"N"`
-		S [][]float64 `bson:"S" json:"S"`
-		V [][]float64 `bson:"V" json:"V"`
-		F [][]float64 `bson:"F" json:"F"`
-		Q [][]float64 `bson:"Q" json:"Q"`
-	} `bson:"annotation,omitempty" json:"annotation,omitempty"`
-	SamplePlot struct {
-		N []float64 `bson:"N" json:"N"`
-		S []float64 `bson:"S" json:"S"`
-		V []float64 `bson:"V" json:"V"`
-		F []float64 `bson:"F" json:"F"`
-		Q []float64 `bson:"Q" json:"Q"`
-	} `bson:"sample_plot,omitempty" json:"sample_plot,omitempty"`
+	RecordTime string             `bson:"recordTime,omitempty" json:"recordTime,omitempty"`
+	EcgPlot    []float64          `bson:"ecgplot,omitempty" json:"ecgplot,omitempty"`
 }
 
 func (t Topic) CreateTopic(input *validations.CreateTopicWithUSerIDInput) (*Topic, error) {
@@ -120,7 +99,6 @@ func (t Topic) UpdateECGdata(input *validations.UpdateECGInput) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	errChannel := make(chan error)
-
 	go func() {
 		defer close(errChannel)
 		filter := bson.M{"_id": input.TopicID, "userId": input.UserID}
@@ -141,74 +119,117 @@ func (t Topic) UpdateECGdata(input *validations.UpdateECGInput) error {
 	return <-errChannel
 }
 
-func (t Topic) ECGPrediction(input *validations.ECGPredictionInput) error {
+func (t Topic) UpdateRecordTime(input *validations.UpdateRecordTimeVal) error {
 	topicCollection := db.GetTopicCollection()
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	filter := bson.M{"_id": input.TopicID, "userId": input.UserID}
+	_, err := topicCollection.UpdateOne(ctx, filter, bson.M{"$set": bson.M{"recordTime": input.RecordTime}})
+	if err != nil {
+		return fmt.Errorf("failed to update record time: %v", err)
+	}
+	return nil
+}
+
+func (t Topic) ECGPrediction(input *validations.ECGPredictionInput) (*Prediction, error) {
+	topicCollection := db.GetTopicCollection()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	filter := bson.M{"_id": input.TopicID, "userId": input.UserID}
+
 	var topic Topic
 	err := topicCollection.FindOne(ctx, filter).Decode(&topic)
 	if err != nil {
-		return fmt.Errorf("failed to retrieve topic with ID %v: %v", input.TopicID, err)
+		return nil, fmt.Errorf("failed to retrieve topic with ID %v: %v", input.TopicID, err)
 	}
 
 	packet := validations.Payload{
 		ECGPlot: topic.EcgPlot,
+		Feature: input.Feature,
 	}
 
 	jsondata, err := json.Marshal(packet)
 	if err != nil {
-		return fmt.Errorf("failed to marshal data: %v", err)
+		return nil, fmt.Errorf("failed to marshal data: %v", err)
 	}
+
 	if err := godotenv.Load(); err != nil {
 		panic("Failed to load .env file!")
 	}
+
 	resp, err := http.Post(os.Getenv("MODEL_URL"), "application/json", bytes.NewBuffer(jsondata))
 	if err != nil {
-		return fmt.Errorf("failed to send request: %v", err)
+		return nil, fmt.Errorf("failed to send request: %v", err)
 	}
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalf("Error reading response body: %v", err)
 	}
 
 	var predictionResult struct {
-		N          float64       `json:"N"`
-		S          float64       `json:"S"`
-		V          float64       `json:"V"`
-		F          float64       `json:"F"`
-		Q          float64       `json:"Q"`
-		Feature    string        `json:"feature"`
-		SampleList [][]float64   `json:"sample_list"`
-		Annotation [][][]float64 `json:"annotation"`
+		N             float64       `json:"N"`
+		S             float64       `json:"S"`
+		V             float64       `json:"V"`
+		F             float64       `json:"F"`
+		Q             float64       `json:"Q"`
+		Feature       string        `json:"feature"`
+		SampleList    [][]float64   `json:"sample_list"`
+		Annotation    [][][]float64 `json:"annotation"`
+		InferenceTime float64       `json:"inferenceTime"`
+		ModelVersion  string        `json:"modelVersion"`
 	}
 
 	err = json.Unmarshal(body, &predictionResult)
 	if err != nil {
-		return fmt.Errorf("failed to unmarshal prediction result: %v", err)
+		return nil, fmt.Errorf("failed to unmarshal prediction result: %v", err)
 	}
-
-	_, err = topicCollection.UpdateOne(ctx, filter, bson.M{"$set": bson.M{
-		"prediction.N":  predictionResult.N,
-		"prediction.S":  predictionResult.S,
-		"prediction.V":  predictionResult.V,
-		"prediction.F":  predictionResult.F,
-		"prediction.Q":  predictionResult.Q,
-		"feature":       predictionResult.Feature,
-		"sample_plot.N": predictionResult.SampleList[0],
-		"sample_plot.S": predictionResult.SampleList[1],
-		"sample_plot.V": predictionResult.SampleList[2],
-		"sample_plot.F": predictionResult.SampleList[3],
-		"sample_plot.Q": predictionResult.SampleList[4],
-		"annotation.N":  predictionResult.Annotation[0],
-		"annotation.S":  predictionResult.Annotation[1],
-		"annotation.V":  predictionResult.Annotation[2],
-		"annotation.F":  predictionResult.Annotation[3],
-		"annotation.Q":  predictionResult.Annotation[4],
-	}})
-	if err != nil {
-		return fmt.Errorf("failed to update prediction: %v", err)
+	createPrediction := &Prediction{
+		TopicID:       input.TopicID,
+		InferenceTime: predictionResult.InferenceTime,
+		Prediction: struct {
+			N float64 `bson:"N" json:"N"`
+			S float64 `bson:"S" json:"S"`
+			V float64 `bson:"V" json:"V"`
+			F float64 `bson:"F" json:"F"`
+			Q float64 `bson:"Q" json:"Q"`
+		}{
+			N: predictionResult.N,
+			S: predictionResult.S,
+			V: predictionResult.V,
+			F: predictionResult.F,
+			Q: predictionResult.Q,
+		},
+		Feature: predictionResult.Feature,
+		Annotation: struct {
+			N [][]float64 `bson:"N" json:"N"`
+			S [][]float64 `bson:"S" json:"S"`
+			V [][]float64 `bson:"V" json:"V"`
+			F [][]float64 `bson:"F" json:"F"`
+			Q [][]float64 `bson:"Q" json:"Q"`
+		}{
+			N: predictionResult.Annotation[0],
+			S: predictionResult.Annotation[1],
+			V: predictionResult.Annotation[2],
+			F: predictionResult.Annotation[3],
+			Q: predictionResult.Annotation[4],
+		},
+		SamplePlot: struct {
+			N []float64 `bson:"N" json:"N"`
+			S []float64 `bson:"S" json:"S"`
+			V []float64 `bson:"V" json:"V"`
+			F []float64 `bson:"F" json:"F"`
+			Q []float64 `bson:"Q" json:"Q"`
+		}{
+			N: predictionResult.SampleList[0],
+			S: predictionResult.SampleList[1],
+			V: predictionResult.SampleList[2],
+			F: predictionResult.SampleList[3],
+			Q: predictionResult.SampleList[4],
+		},
+		ModelVersion: predictionResult.ModelVersion,
 	}
-	return nil
+	return createPrediction, nil
 }
