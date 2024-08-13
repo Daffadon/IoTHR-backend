86becbb838a159fa38a7db9f7db91985d5d459e0
Daffadon <daffaputranarendra9@gmail.com>
2024-08-13 09:24:26 +0700

    feat: add new controller function to fill doctor functionality

--- controllers/user-controller.go
+++ controllers/user-controller.go
@@ -10,12 +10,58 @@ import (
 
 type UserController struct{}
 
+func (u UserController) GetUsers(ctx *gin.Context) {
+	users, err := UserModel.GetUsers()
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.Abort()
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"data": users})
+}
+
+func (u UserController) GetUser(ctx *gin.Context) {
+	userId, err := primitive.ObjectIDFromHex(ctx.Param("userId"))
+	if err == nil {
+		user, err := UserModel.GetUserByID(userId)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			ctx.Abort()
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{"data": user})
+		ctx.Abort()
+		return
+	}
+	ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid user"})
+}
+
+func (u UserController) GetUserHistory(ctx *gin.Context) {
+	userId, err := primitive.ObjectIDFromHex(ctx.Param("userId"))
+	if err == nil {
+		if user, err := UserModel.GetUserByID(userId); err == nil {
+			topicList, err := TopicModel.GetUserTopicList(user.TopicID)	
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				ctx.Abort()
+				return
+			}
+			ctx.JSON(http.StatusOK, gin.H{"data": topicList})
+			ctx.Abort()
+			return
+		}
+	}
+	ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid user"})
+}
+
 func (u UserController) GetProfile(ctx *gin.Context) {
 	userID, ok := ctx.Get("user_id")
 	if ok {
 		if user, err := UserModel.GetUserByID(userID.(primitive.ObjectID)); err == nil {
 			userData := &validations.Profile{
-				Email: user.Email, Fullname: user.Fullname,
+				Email:     user.Email,
+				Fullname:  user.Fullname,
+				BirthDate: user.BirthDate,
 			}
 			ctx.JSON(http.StatusOK, gin.H{"data": userData})
 			ctx.Abort()
@@ -29,7 +75,6 @@ func (u UserController) GetHistory(ctx *gin.Context) {
 	userID, ok := ctx.Get("user_id")
 	if ok {
 		if user, err := UserModel.GetUserByID(userID.(primitive.ObjectID)); err == nil {
-
 			topicNames, err := TopicModel.GetTopicList(&validations.GetHistoryInput{TopicList: user.TopicID})
 			if err != nil {
 				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
